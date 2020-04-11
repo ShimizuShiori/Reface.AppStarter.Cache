2b6653d17f0dabdb6f2fe7617310ed04d3f5a87c
ShimizuShiori <116971263@qq.com>
2020-04-11 22:33:51 +0800

    将控制台打印改为Debug打印

--- src/Reface.AppStarter.Cache/Reface.AppStarter.Cache/DefaultCachePool.cs
+++ src/Reface.AppStarter.Cache/Reface.AppStarter.Cache/DefaultCachePool.cs
@@ -13,21 +13,21 @@ namespace Reface.AppStarter.Cache
 
         public void Clean(string key)
         {
-            Console.WriteLine($"[{Thread.CurrentThread.Name}] Cache.CLN\t[{key}]");
+            Debug.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Cache.CLN\t[{key}]");
             object value;
             cachePool.TryRemove(key, out value);
         }
 
         public void Set(string key, object value)
         {
-            Console.WriteLine($"[{Thread.CurrentThread.Name}] Cache.SET\t[{key}] = [{value}]");
+            Debug.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Cache.SET\t[{key}] = [{value}]");
             cachePool[key] = value;
         }
 
         public bool TryGet(string key, out object value)
         {
             bool result = cachePool.TryGetValue(key, out value);
-            Console.WriteLine($"[{Thread.CurrentThread.Name}] Cache.TGT\t[{key}] : {result}");
+            Debug.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Cache.TGT\t[{key}] : {result}");
             return result;
         }
     }
